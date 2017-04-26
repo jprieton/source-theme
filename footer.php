4ca19760302ffaad91aa1cc4a6208a83c5a22250
Javier Prieto <jprieton@gmail.com>
2017-04-26 16:13:56 -0430

    Header & Footer

--- footer.php
+++ footer.php
@@ -1,17 +1,36 @@
-    <?php
-    /**
-     * If this file is called directly, abort.
-     */
-    if ( !defined( 'ABSPATH' ) ) {
-      die( 'Direct access is forbidden.' );
-    }
+<?php
+/**
+ * If this file is called directly, abort.
+ */
+if ( !defined( 'ABSPATH' ) ) {
+  die( 'Direct access is forbidden.' );
+}
 
+/**
+ * This is the template that displays all of the <footer> section and everything after main content
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package     WordPress
+ * @subpackage  SourceTheme
+ * @since       1.0.0
+ * @version     1.0.0
+ */
+?>
+
+    <footer id="site-footer">
+
+    </footer>
+
+    <?php
     wp_footer();
+
     /**
-     * This hook is called after the main content is rendered
-     * @since 1.0.0
-     */
+    * This hook is called after the main content is rendered
+    * @since 1.0.0
+    */
     do_action( 'after_main_content' );
     ?>
+
   </body>
 </html>
--- header.php
+++ header.php
@@ -5,13 +5,23 @@
 if ( !defined( 'ABSPATH' ) ) {
   die( 'Direct access is forbidden.' );
 }
+
+/**
+ * This is the template that displays all of the <head> section and everything before main content
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package     WordPress
+ * @subpackage  SourceTheme
+ * @since       1.0.0
+ * @version     1.0.0
+ */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 
   <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <?php wp_head() ?>
@@ -19,6 +29,11 @@ if ( !defined( 'ABSPATH' ) ) {
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <noscript>
+        <strong>Warning !</strong>
+        Because your browser does not support HTML5, some elements are simulated using javascript.
+        Unfortunately your browser has disabled scripting. Please enable it in order to display correctly this page.
+      </noscript>
     <![endif]-->
 
   </head>
@@ -32,3 +47,7 @@ if ( !defined( 'ABSPATH' ) ) {
      */
     do_action( 'before_main_content' );
     ?>
+
+    <header id="#site-header">
+
+    </header>
