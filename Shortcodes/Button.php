8cbbf23558809b461592e4570e56297d00ff2684
Javier Prieto <jprieton@gmail.com>
2017-05-08 16:55:52 -0430

    Buttons shortcodes

---
+++ Shortcodes/Button.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SourceTheme\Shortcodes;
+
+/**
+ * If this file is called directly, abort.
+ */
+if ( !defined( 'ABSPATH' ) ) {
+  die( 'Direct access is forbidden.' );
+}
+
+/**
+ * Description of Button
+ *
+ * @author perseo
+ */
+class Button {
+
+  public function __construct() {
+    
+  }
+
+  public static function btn() {
+
+  }
+
+  public static function btn_default() {
+
+  }
+
+  public static function btn_primary() {
+
+  }
+
+  public static function btn_success() {
+
+  }
+
+  public static function btn_info() {
+
+  }
+
+  public static function btn_warning() {
+
+  }
+
+  public static function btn_danger() {
+
+  }
+
+  public static function btn_link() {
+
+  }
+
+}
