d216f84bf4cc0dd97c9d36e789c1881b4f71d5d8
Javier Prieto <jprieton@gmail.com>
2017-05-10 12:01:11 -0430

    Cleanup

---
+++ Template/Form.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SourceTheme\Template;
+
+/**
+ * If this file is called directly, abort.
+ */
+if ( !defined( 'ABSPATH' ) ) {
+  die( 'Direct access is forbidden.' );
+}
+
+/**
+ * SourceTheme class
+ *
+ * @package     SourceTheme
+ * @subpackage  Template
+ *
+ * @author      Javier Prieto <jprieton@gmail.com>
+ * @copyright	  Copyright (c) 2017, Javier Prieto
+ * @since       1.0.0
+ * @license     http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+class Form {
+  //put your code here
+}
